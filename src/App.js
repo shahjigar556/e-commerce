8db4cb3313d97dbfe7294b27bd965468f4df421c
Jigar <shahjigar185@gmail.com>
2020-10-27 18:13:01 +0530

    developed the basic components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
-import './App.css';
+import './data';
+// importing components
+import React from 'react'
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';// list of products
+import Cart from './components/Cart';
+import Footer from './components/Footer';
+import {Route,Switch,Link } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <Navbar />
+          
+      </Router>
   );
 }
 
