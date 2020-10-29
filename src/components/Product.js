2569d35afdd74a33ef2e1d32a42ebf400c1e81b6
Jigar <shahjigar185@gmail.com>
2020-10-29 05:35:10 +0530

    developed the product dashboard without using context-API

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,33 @@
 import React from 'react'
-
-export default function Product(props) {
-    console.log("In products")
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import {Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button'
+export default function Product({product}) {
+    const url=`/details/${product.id}`
+    const LinkStyle={
+        textDecoration:'none',
+        color:'black'
+    }
     return (
-        <div>
-            <h3>{props.product.id}</h3>
-        </div>
+        <Grid item xs={12} sm={6} md={4} >
+             <Paper >
+                 <h3>
+                    {product.title}
+                 </h3>
+                 <img className="img-fluid" src={product.img} alt="img"></img>
+                 <h4>
+                    {product.company}
+                 </h4>
+                 <h4>
+                     {product.price} $
+                 </h4>
+                 <center>
+                 <Button variant="contained" color="secondary" className="text-center mb-3">
+                     <Link to={url} style={LinkStyle}>Click For More</Link>
+                 </Button>
+                 </center>
+            </Paper>
+        </Grid>    
     )
 }
--- src/views/ProductList.js
+++ src/views/ProductList.js
@@ -4,12 +4,16 @@ import {useState,useEffect} from 'react';
 import {storeProducts} from '../data';
 import Grid from '@material-ui/core/Grid';
 import Title from '../components/Title';
+
 export default function ProductList() {
     const [products,setProducts]=useState(storeProducts);// creating state of products
     console.log(products);
     return (
      <div className="container">
          <Title name="Our" title="Products" />
+         <Grid  container spacing={2}>
+             {products.map(product=><Product key={product.id} product={product} />)}
+         </Grid>
         <div className='row'>
             
         </div>
